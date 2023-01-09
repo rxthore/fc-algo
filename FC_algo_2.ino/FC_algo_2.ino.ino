9933be24d553d0108edaa792c774797ed7e5bd52
rxthore@gmail.com <59700645+rxthore@users.noreply.github.com>
2023-01-09 02:33:04 -0800

    visualiser

--- FC_algo_2.ino/FC_algo_2.ino.ino
+++ FC_algo_2.ino/FC_algo_2.ino.ino
@@ -1,7 +1,6 @@
 #include <Wire.h> // Include the Wire library
 #include <I2Cdev.h> // Include the I2Cdev library
 #include <MPU6050.h> // Include the MPU6050 library
-#define DT 0.01
 
 MPU6050 mpu; // Create a MPU6050 object
 
@@ -17,15 +16,15 @@ float GyX_cal, GyY_cal, GyZ_cal;
 float AcX_offset, AcY_offset, AcZ_offset;
 float GyX_offset, GyY_offset, GyZ_offset;
 
-// LowPass filter variables
-float gx_filtered, gy_filtered, gz_filtered = 0.0;
-float ax_filtered, ay_filtered, az_filtered = 0.0;
-
 // number of samples to use for calibration
-int num_samples = 2000;
+int num_samples = 5000;
 
-float roll = 0.0;
+float angleX = 0.0;
+float angleY = 0.0;
+float angleZ = 0.0;
+
 float pitch = 0.0;
+float roll = 0.0;
 float yaw = 0.0;
 
 void setup() {
@@ -92,8 +91,6 @@ void setup() {
     digitalWrite(13, LOW);
     delay(250);
   }
-
-  Serial.println("Roll:, Pitch:, Yaw:");
 }
 
 void loop() {
@@ -105,51 +102,35 @@ int imu_read() {
   digitalWrite(13, HIGH);
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-  // calculate calibrated readings
-  // convert accelerometer readings to g's
-  AcX_cal = (ax - AcX_offset) / 16384;
-  AcY_cal = (ay - AcY_offset) / 16384;
-  AcZ_cal = (az - AcZ_offset) / 16384;
-  // convert gyro readings to radians per second
-  GyX_cal = ((gx - GyX_offset) / 100) * (PI / 180);
-  GyY_cal = ((gy - GyY_offset) / 100) * (PI / 180);
-  GyZ_cal = ((gz - GyZ_offset) / 100) * (PI / 180);
+  AcX_cal = ax - AcX_offset;
+  AcY_cal = ay - AcY_offset;
+  AcZ_cal = az - AcZ_offset;
+  GyX_cal = gx - GyX_offset;
+  GyY_cal = gy - GyY_offset;
+  GyZ_cal = gz - GyZ_offset;
 
-  applyLowPassFilter();
-  // applyComplementaryFilter();  
+  // Convert the acceleration values to g's
+  float axf = AcX_cal / 16384.0;
+  float ayf = AcY_cal / 16384.0;
+  float azf = AcZ_cal / 16384.0;
 
-  float tempDt = 0.01;
+  // Convert the gyro values to radians/second
+  float gxf = GyX_cal * (PI/180);
+  float gyf = GyY_cal * (PI/180);
+  float gzf = GyZ_cal * (PI/180);
 
-  roll += GyY_cal * tempDt;
-  pitch += GyX_cal * tempDt;
-  yaw += GyZ_cal * tempDt;
+  // Calculate the pitch and roll angles using a complementary filter
+  float alpha = 0.96;
+  pitch = alpha * (pitch + gyf * 0.01) + (1.0 - alpha) * atan2(ayf, azf);
+  roll = alpha * (roll + gxf * 0.01) + (1.0 - alpha) * atan2(-axf, sqrt(ayf * ayf + azf * azf));
 
-  Serial.print(roll);
-  Serial.print(",");
+  // Print the pitch, roll, and yaw angles
+  // Serial.println("Pitch: ");
   Serial.print(pitch);
-  Serial.print(",");
+  // Serial.print("  Roll: ");
+  Serial.print(roll);
+  // Serial.print("  Yaw: ");
   Serial.println(yaw);
 
-  delay(100);
-}
-
-void applyLowPassFilter() {
-  float alpha = 0.8;
-  // apply lowpass on gyro
-  gx_filtered = alpha * gx_filtered + (1.0 - alpha) * GyX_cal;
-  gy_filtered = alpha * gy_filtered + (1.0 - alpha) * GyY_cal;
-  gz_filtered = alpha * gz_filtered + (1.0 - alpha) * GyZ_cal;
-  // apply lowpass on accelerometer
-  ax_filtered = alpha * ax_filtered + (1.0 - alpha) * AcX_cal;
-  ay_filtered = alpha * ay_filtered + (1.0 - alpha) * AcY_cal;
-  az_filtered = alpha * az_filtered + (1.0 - alpha) * AcZ_cal;
-}
-
-void applyComplementaryFilter() {
-  float alpha = 0.98;
-  float dt = 0.01;
-
-  roll = alpha*(roll + GyY_cal*dt) + (1-alpha)*atan2(AcY_cal, AcZ_cal);
-  pitch = alpha*(pitch + GyX_cal*dt) + (1-alpha)*atan2(-AcX_cal, sqrt(AcY_cal*AcY_cal + AcZ_cal*AcZ_cal));
-  yaw = alpha*(yaw + GyZ_cal*dt) + (1-alpha)*atan2(AcY_cal, AcX_cal);
+  delay(10); // Delay for 250 milliseconds
 }
